d858e42ccb1626a75f78c91cbacfbc181718855a
megan-allsop <38864406+megan-allsop@users.noreply.github.com>
2018-07-16 11:04:21 -0600

    a

---
+++ homework/responsive_media/js/functions.js
@@ -0,0 +1,12 @@
+/* *************************************
+*  Weather Site JavaScript Functions
+************************************* */
+
+console.log('My javascript is being read.');
+const TEMP = 31;
+const SPEED = 5;
+const DIRECTION = 'NNW' 
+
+function getCondtion() {
+    
+}
